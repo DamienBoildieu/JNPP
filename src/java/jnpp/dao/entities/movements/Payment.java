abf2fd046542c540c30d68259d58a8ce076cc64e
pbourqua <pierre.bourquat@etu.univ-poitiers.fr>
2018-10-28 04:31:54 +0100

    Ajout des paiments.

--- src/java/jnpp/dao/entities/movements/Payment.java
+++ src/java/jnpp/dao/entities/movements/Payment.java
@@ -4,23 +4,30 @@ import java.io.Serializable;
 
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import jnpp.dao.entities.PaymentMean;
 
 import jnpp.dao.entities.accounts.Currency;
 
 @Entity
-@DiscriminatorValue(value = Movement.Type.Values.WITHDRAW)
-public class Withdraw extends Movement implements Serializable {
+@DiscriminatorValue(value = Movement.Type.Values.PAYMENT)
+public class Payment extends Movement implements Serializable {
 
     private static final long serialVersionUID = 1L;
-
+    
     private Double money;
     private Currency currency;
     
-    public Withdraw() {}
+    @ManyToOne
+    @JoinColumn(name="paymentmean_fk")
+    private PaymentMean paymentMean;
+    
+    public Payment() {}
     
     @Override
     public Type getType() {
-        return Movement.Type.WITHDRAW;
+        return Movement.Type.PAYMENT;
     }
 
     public Double getMoney() {
@@ -38,10 +45,18 @@ public class Withdraw extends Movement implements Serializable {
     public void setCurrency(Currency currency) {
         this.currency = currency;
     }
+
+    public PaymentMean getPaymentMean() {
+        return paymentMean;
+    }
+
+    public void setPaymentMean(PaymentMean paymentMean) {
+        this.paymentMean = paymentMean;
+    }
     
     @Override
     public String toString() {
-        return "jnpp.dao.entities.transactions.Withdraw[ id=" + getId() + " ]";
+        return "jnpp.dao.entities.movements.Paiement[ id=" + getId() + " ]";
     }
     
 }
